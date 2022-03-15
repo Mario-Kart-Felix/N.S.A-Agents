62d1606af8b05f540da3eeb0b9f8beecf49389dc
Phantom <76971465+Mario-Kart-Felix@users.noreply.github.com>
2022-03-15 16:11:32 -0500

    Create N.s.s

---
+++ .github/workflows/N.s.s
@@ -0,0 +1,93 @@
+# Title
+
+ National Defense. PART 67—EDUCATIONAL REQUIREMENTS FOR APPOINTMENT OF RESERVE COMPONENT OFFICERS TO A GRADE ABOVE FIRST LIEUTENANT OR LIEUTENANT (JUNIOR GRADE)
+
+
+# ID
+
+ CFR-2018-title32-vol1.Pt. 67
+
+
+# Structured Analysis Summary
+
+| Type        | Values                                                                                                                                                                                            |
+|:------------|:--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| Constraints | ['no later than', 'at least', 'before', 'after', 'more than']                                                                                                                                     |
+| Duration    | ['8.0 year']                                                                                                                                                                                      |
+| Condition   | ['if']                                                                                                                                                                                            |
+| Entities    | ['United States', 'Juneau, Alaska', 'Lieutenant', 'Staff', 'Seaman', 'Purpose', 'Washington, DC', 'Anchorage', 'Captain', 'Policy', 'Education', 'Applicability', 'Defense', 'Reserve', 'Alaska'] |
+| Date        | ['1995-09-30', '2018-10-01', '2018-01-01']                                                                                                                                                        |
+
+
+# Structured Analysis With Context
+
+ 
+
+
+## Constraints
+
+| Constraints   | Context                                                                                                                            |
+|:--------------|:-----------------------------------------------------------------------------------------------------------------------------------|
+| at least      | 12205 to require Reserve component officers to have at least a baccalaureate degree from a qualifying educational institution      |
+| before        | a baccalaureate degree from a qualifying educational institution before appointment to a grade above First Lieutenant in           |
+| before        | or, in the case of the Navy, lieutenant before  October 1, 1995.                                                                   |
+| more than     | permanently at a location in Alaska that is more than 50 miles from each of the cities of                                          |
+| after         | Military Departments shall establish procedures to ensure that after September 30, 1995, those Reserve component officers selected |
+| before        | a baccalaureate degree from a qualifying educational institution before  appointment to the next higher grade.                     |
+| more than     | 12205, the degree must not have been awarded more than 8 years before the date the officer is                                      |
+| before        | not have been awarded more than 8 years before the date the officer is to be appointed,                                            |
+| at least      | ROTC programs that their educational institutions would accept at least 90 percent of the credit hours earned by                   |
+| at least      | study in which that educational institution would accept at least 90 percent of the credit hours earned by                         |
+| no later than | unaccredited educational institution must submit the required documentation no later than January 1 of the year for which the      |
+
+
+## Duration
+
+| Duration   | Context                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  |
+|:-----------|:-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| 8.0 year   | For a degree from an unaccredited educational institution that has been recognized as qualifying educational institution by the Department of Defense to satisfy the educational requirements of 10 U.S.C. 12205, the degree must not have been awarded more than 8 years before the date the officer is to be appointed, or federally recognized, in the grade of Captain in the Army Reserve, Army National Guard, Air Force Reserve, Air National Guard, or Marine Corps Reserve, or in the grade of Lieutenant in the Naval Reserve. |
+
+
+## Condition
+
+| Condition   | Context                                                                                                                |
+|:------------|:-----------------------------------------------------------------------------------------------------------------------|
+| if          | Appointed to or recognized in a higher grade if appointed to, or federally recognized in, the grade                    |
+| if          | designate an unaccredited educational institution as a qual if ying educational institution for the purpose of meeting |
+
+
+## Entities
+
+| Entities       | Context                                                                                                                 |
+|:---------------|:------------------------------------------------------------------------------------------------------------------------|
+| Purpose        | Purpose .                                                                                                               |
+| Lieutenant     | appointment of officers to a grade above First Lieutenant in the Army Reserve, Air Force Reserve, and                   |
+| Applicability  | Applicability .                                                                                                         |
+| Defense        | applies to the Office of the Secretary of Defense , and the Military Departments; the Chairman of                       |
+| Staff          | Departments; the Chairman of the Joint Chiefs or Staff ; and the Defense Agencies referred to collectively              |
+| Reserve        | The term &#8220; Reserve  components&#8221; refers to the Army                                                          |
+| United States  | the Army Reserve, Army National Guard of the United States , Air Force Reserve, Air National Guard of                   |
+| United States  | the Army Reserve, Army National Guard of the United States , Air Force Reserve, Air National Guard of                   |
+| Education      | by an agency recognized by the Secretary of Education .                                                                 |
+| Education      | by an agency recognized by the Secretary of Education .                                                                 |
+| Policy         | Policy .                                                                                                                |
+| Lieutenant     | institution before appointment to a grade above First Lieutenant in the Army Reserve, Air Force Reserve or              |
+| Seaman         | the Naval Aviation Cadet (NAVCAD) program or the Seaman  to Admiral program.                                            |
+| Alaska         | the grade of captain or major in the Alaska Army National Guard, who resides permanently at a                           |
+| Anchorage      | 50 miles from each of the cities of Anchorage , Fairbanks, and Juneau, Alaska, by paved road,                           |
+| Juneau, Alaska | each of the cities of Anchorage, Fairbanks, and Juneau, Alaska , by paved road, and who is serving                      |
+| Defense        | that have been approved by the Department of Defense  as a qualifying educational institution.                          |
+| Lieutenant     | selected for appointment to a grade above First Lieutenant in the Army Reserve, Air Force Reserve, or                   |
+| Defense        | as qualifying educational institution by the Department of Defense to satisfy the educational requirements of 10 U.S.C. |
+| Captain        | appointed, or federally recognized, in the grade of Captain in the Army Reserve, Army National Guard, Air               |
+| Washington, DC | Reserve Affairs, Attn: DASD (M&amp;P), 1500 Defense Pentagon, Washington, DC  20301-1500.                               |
+
+
+## Date
+
+| Date       | Context                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    |
+|:-----------|:-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| 2018-10-01 | (4) Appointed to or recognized in a higher grade if appointed to, or federally recognized in, the grade of captain or, in the case of the Navy, lieutenant before October 1, 1995.                                                                                                                                                                                                                                                                                                                                                                                                                                                                         |
+| 2017-09-30 | (b) The Secretaries of the Military Departments shall establish procedures to ensure that after September 30, 1995, those Reserve component officers selected for appointment to a grade above First Lieutenant in the Army Reserve, Air Force Reserve, or Marine Corps Reserve, or Lieutenant (Junior Grade) in the Naval Reserve, or for officers to be federally recognized in a grade above First Lieutenant as a member of the Army National Guard or Air National Guard, who are required to hold a baccalaureate degree, were awarded a baccalaureate degree from a qualifying educational institution before appointment to the next higher grade. |
+| 2022-01-01 | (c) For an unaccredited educational institution to be considered for designation as a qualifying educational institution, the unaccredited educational institution must submit the required documentation no later than January 1 of the year for which the unaccredited educational institution seeks to be designated a qualifying educational institution.                                                                                                                                                                                                                                                                                              |
+
